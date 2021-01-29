ce4034c100aa4539a65a11ab620fab9802740eb8
Nils van der Velden <nilsepils123@gmail.com>
2021-01-29 15:34:47 +0100

    Done with Refactoring cityDTO

--- src/main/java/com/groep6/pfor/util/parsers/templates/BoardDTO.java
+++ src/main/java/com/groep6/pfor/util/parsers/templates/BoardDTO.java
@@ -1,9 +1,6 @@
 package com.groep6.pfor.util.parsers.templates;
 
-import com.groep6.pfor.factories.FactionFactory;
 import com.groep6.pfor.models.*;
-import com.groep6.pfor.models.factions.Faction;
-import com.groep6.pfor.models.factions.FactionType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,7 +22,7 @@ public class BoardDTO {
 
     public Board toModel() {
         Map<String, Tile> cities = new HashMap<>();
-        for (CityDTO city : this.cities.values()) cities.put(city.name, city.toModel());
+        for (CityDTO city : this.cities.values()) cities.put(city.cityName, city.parseToModel());
         
         return new Board(cities);
     }
@@ -34,7 +31,7 @@ public class BoardDTO {
         Map<String, CityDTO> cities = new HashMap<>();
         for (Tile tile : board.getTiles()) {
             City city = (City) tile;
-            cities.put(city.getCityName(), CityDTO.fromModel(city));
+            cities.put(city.getCityName(), CityDTO.parseFromModel(city));
         }
         return new BoardDTO(cities);
     }
--- src/main/java/com/groep6/pfor/util/parsers/templates/CityDTO.java
+++ src/main/java/com/groep6/pfor/util/parsers/templates/CityDTO.java
@@ -8,6 +8,7 @@ import com.groep6.pfor.models.factions.Faction;
 import com.groep6.pfor.models.factions.FactionType;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
@@ -17,48 +18,99 @@ import java.util.Stack;
  * @author OwenElderbroek
  */
 public class CityDTO {
-    public String name;
-    public boolean fort;
-    public int legions;
-    public int barbarians;
+    public String cityName;
+    public boolean cityHasFort;
+    public int amountOfLegionsInCity;
+    public int amountOfBarbariansInCity;
 
     public CityDTO() {}
 
-    private CityDTO(String name, boolean fort, int legions, int barbarians) {
-        this.name = name;
-        this.fort = fort;
-        this.legions = legions;
-        this.barbarians = barbarians;
+    private CityDTO(String cityName, boolean cityHasFort, int amountOfLegionsInCity, int amountOfBarbariansInCity) {
+        this.cityName = cityName;
+        this.cityHasFort = cityHasFort;
+        this.amountOfLegionsInCity = amountOfLegionsInCity;
+        this.amountOfBarbariansInCity = amountOfBarbariansInCity;
     }
 
-    public static CityDTO fromModel(City city) {
-        Map<String, Integer> barbarians = new HashMap<>();
-        for (Faction faction : FactionFactory.getInstance().getFactions()) barbarians.put(faction.getFactionType().toString(), 0);
+    private static List<Faction> getFactions() {
+        FactionFactory factionFactory = FactionFactory.getInstance();
+        return factionFactory.getFactions();
+    }
+
+    private static void addBarbariansToGame(Map<String, Integer> barbarians) {
+        for (Faction faction : getFactions()) {
+            barbarians.put(getFactionTypeAsString(faction), 0);
+        }
+    }
+
+    private static FactionType getFactionType(Faction faction) {
+        return faction.getFactionType();
+    }
+
+    private static String getFactionTypeAsString(Faction faction) {
+        return getFactionType(faction).toString();
+    }
+
+    private static FactionType getFactionTypeOfBarbarian(Barbarian barbarian) {
+        return barbarian.getFactionType();
+    }
+
+    private static  String getFactionTypeOfBarbarianAsString(Barbarian barbarian) {
+        return getFactionTypeOfBarbarian(barbarian).toString();
+    }
+
+    private static int getNextBarbarianInteger(Map<String, Integer> barbarians, Barbarian barbarian) {
+        return barbarians.get(getFactionTypeOfBarbarianAsString(barbarian)) + 1;
+    }
+
+    private static void addBarbariansToCities(Map<String, Integer> barbarians, City city) {
         for (Barbarian barbarian : city.getBarbariansInCity())
-            barbarians.put(barbarian.getFactionType().toString(), barbarians.get(barbarian.getFactionType().toString()) + 1);
-
-        int barbs = barbarians.get(FactionType.VISIGOTHS.toString());
-        barbs |= barbarians.get(FactionType.VANDALS.toString()) * 64;
-        barbs |= (int) (barbarians.get(FactionType.ANGLO_SAXSONS_FRANKS.toString()) * Math.pow(64, 2));
-        barbs |= (int) (barbarians.get(FactionType.HUNS.toString()) * Math.pow(64, 3));
-        barbs |= (int) (barbarians.get(FactionType.OSTROGOTHS.toString()) * Math.pow(64, 4));
-        return new CityDTO(city.getCityName(), city.hasFort(), city.getLegionCount(), barbs);
+            barbarians.put(getFactionTypeOfBarbarianAsString(barbarian), getNextBarbarianInteger(barbarians, barbarian));
     }
 
-    public City toModel() {
+    public static CityDTO parseFromModel(City city) {
+
+        Map<String, Integer> barbarians = new HashMap<>();
+
+        addBarbariansToGame(barbarians);
+
+        addBarbariansToCities(barbarians, city);
+
+        int amountOfBarbarians = barbarians.get(FactionType.VISIGOTHS.toString());
+        amountOfBarbarians |= barbarians.get(FactionType.VANDALS.toString()) * 64;
+        amountOfBarbarians |= (int) (barbarians.get(FactionType.ANGLO_SAXSONS_FRANKS.toString()) * Math.pow(64, 2));
+        amountOfBarbarians |= (int) (barbarians.get(FactionType.HUNS.toString()) * Math.pow(64, 3));
+        amountOfBarbarians |= (int) (barbarians.get(FactionType.OSTROGOTHS.toString()) * Math.pow(64, 4));
+
+        return new CityDTO(city.getCityName(), city.hasFort(), city.getLegionCount(), amountOfBarbarians);
+    }
+
+    private void addLegion(Stack<Legion> legions) {
+        legions.push(new Legion());
+    }
+
+    public City parseToModel() {
         Stack<Legion> legions = new Stack<>();
-        for (int i = 0; i < this.legions; i++) legions.push(new Legion());
+        for (int i = 0; i < this.amountOfLegionsInCity; i++) {
+            addLegion(legions);
+        }
         Stack<Barbarian> barbarians = new Stack<>();
         // Use fancy bitfields to save immense amounts of strain on the database
-        addBarbarians(FactionType.VISIGOTHS, this.barbarians & 63, barbarians);
-        addBarbarians(FactionType.VANDALS, (this.barbarians & (63 << 6)) >> 6, barbarians);
-        addBarbarians(FactionType.ANGLO_SAXSONS_FRANKS, (this.barbarians & (63 << 12)) >> 12, barbarians);
-        addBarbarians(FactionType.HUNS, (this.barbarians & (63 << 18)) >> 18, barbarians);
-        addBarbarians(FactionType.OSTROGOTHS, (this.barbarians & (63 << 24)) >> 24, barbarians);
-        return new City(name, legions, barbarians, fort);
+        addBarbariansToGame(FactionType.VISIGOTHS, this.amountOfBarbariansInCity & 63, barbarians);
+        addBarbariansToGame(FactionType.VANDALS, (this.amountOfBarbariansInCity & (63 << 6)) >> 6, barbarians);
+        addBarbariansToGame(FactionType.ANGLO_SAXSONS_FRANKS, (this.amountOfBarbariansInCity & (63 << 12)) >> 12, barbarians);
+        addBarbariansToGame(FactionType.HUNS, (this.amountOfBarbariansInCity & (63 << 18)) >> 18, barbarians);
+        addBarbariansToGame(FactionType.OSTROGOTHS, (this.amountOfBarbariansInCity & (63 << 24)) >> 24, barbarians);
+        return new City(cityName, legions, barbarians, cityHasFort);
     }
 
-    private void addBarbarians(FactionType faction, int count, Stack<Barbarian> stack) {
-        for (int i = 0; i < count; i++) stack.push(new Barbarian(faction));
+    private void addBarbarian(Stack<Barbarian> barbarians, FactionType factionType) {
+        barbarians.push(new Barbarian(factionType));
+    }
+
+    private void addBarbariansToGame(FactionType faction, int amountOfBarbariansInCity, Stack<Barbarian> barbarians) {
+        for (int i = 0; i < amountOfBarbariansInCity; i++) {
+            addBarbarian(barbarians, faction);
+        }
     }
 }
