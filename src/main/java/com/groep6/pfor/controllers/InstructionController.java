f4bf90940afb72c8c145d6ef0c7f1819afca558c
Nils van der Velden <nilsepils123@gmail.com>
2021-01-30 11:29:34 +0100

    added description to Instruc

--- src/main/java/com/groep6/pfor/controllers/InstructionController.java
+++ src/main/java/com/groep6/pfor/controllers/InstructionController.java
@@ -3,6 +3,13 @@ package com.groep6.pfor.controllers;
 import com.groep6.pfor.util.IObserver;
 import com.groep6.pfor.views.InstructionView;
 
+/**
+ * Controller for showing game instructions a game.
+ * This controller handles showing instructions.
+ * @author Nils van der Velden
+ *
+ */
+
 public class InstructionController extends Controller {
 
     public InstructionController() {
